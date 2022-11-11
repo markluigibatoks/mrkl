274d6ce8c547021168851a14e9fd1c83d6334b31
MARK-LUIGI BATOCTOY <markluigibatoks@gmail.com>
2022-11-11 17:35:04 +0800

    chore: test playgroundview

--- src/components/PlaygroundView.vue
+++ src/components/PlaygroundView.vue
@@ -1,8 +1,15 @@
 <script setup>
-console.log('hi')
+import HeaderView from "@/components/HeaderView.vue";
 </script>
 
 <template>
-  <h1>Welcome to Playground</h1>
-  <router-view/>
+  <header-view />
+  <main>
+    <router-link :to="{ name: 'MatchingPair' }">
+      <div class="mt-20 h-[250px] w-[320px] bg-slate-100 text-center">
+        Matching Pair Game
+      </div>
+    </router-link>
+  </main>
+  <router-view />
 </template>
